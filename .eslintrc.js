ab2b46687f2d103afbd325c63ec22926c63cec47
Pyroarsonist <velikiydan@gmail.com>
2024-05-11 02:42:42 +0300

    fixed npm conflicts
    fixed jsconfig.json name
    
    removed debug pkg

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: "eslint-config-pyroarsonist",
   settings: {
     "import/resolver": {
+      "eslint-import-resolver-custom-alias": {
+        alias: {
+          "~": "./src",
+        },
+      },
       "babel-plugin-root-import": {
         rootPathSuffix: "src",
         rootPathPrefix: "~",
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { isDevelopment, someEnv } from "~/config";
-import debugHandler from "debug";
-
-const debug = debugHandler("app:main");
 
 console.info("Hello world!");
-debug({ someEnv });
-debug("Environment is development:", isDevelopment);
+console.log({ someEnv });
+console.log("Environment is development:", isDevelopment);
